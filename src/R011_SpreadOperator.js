2d59d46349143b25c8afeb443fa8db5fb0ff0128
Parker <zpzpalal88@gmail.com>
2021-07-08 09:42:09 +0900

    R11파일 추가

--- src/R011_SpreadOperator.js
+++ src/R011_SpreadOperator.js
@@ -13,25 +13,31 @@ class R011_SpreadOperator extends Component{
     
 
     componentDidMount(){
-        var varName = 'react';
-        console.log('varName01 : '+varName);
-        var varName = '200'; // var name already defined no redeclare
-        console.log('varName02 : '+varName);
+   //javascript Array
+   var varArray1 = ['num1', 'num2'];
+   var varArray2 = ['num3','num4'];
+   var sumVarArr=[varArray1[0],varArray1[1],varArray2[0],varArray2[1]];
+   //var sumVarArr = [].concat(varArray1,varArray2)
+   console.log('1. sumVarArr :'+sumVarArr);
+   //ES6 Arr
+   let sumLetArr = [...varArray1,...varArray2];
+   console.log('2. sumLetArr : '+sumLetArr);
+   const [sum1,sum2,...remain]=sumLetArr;
+   console.log('3. sum :' +sum1+'sum2:' + sum2+'remain :'+remain);
 
+   var varObj1 = {key1:'var1', key2 :'var2'};
+   var varObj2 = {key2:'new2', key3 : 'val3'};
 
-        let letName = 'react';
-        console.log('letName1 : '+letName);
-        // let letName = 200
-        //parsing error : Identifire 'let Name' has already declare
+   //javascript object
 
-        letName= 'react200';
-        console.log('letName2' +letName);
+   var sumVarObj1 = Object.assign({},varObj1,varObj2);
+   console.log('4.sumVarObj:'+JSON.stringify(sumVarObj1));
+   //ES6 object
 
-
-        const constName = 'react';
-        console.log('constName : '+constName);
-        //const constName =200
-        //parsing error CostNAMe is already declare
+   var sumLetObj = {...varObj1, ...varObj2};
+   console.log('5.sumLetObj : '+JSON.stringify(sumLetObj));
+   var {key1,key3,...others} = sumLetObj;
+   console.log('6. key1 : ' +key1+', key3+'+key3+'others:'+JSON.stringify(others));
 
 
 
@@ -39,7 +45,7 @@ class R011_SpreadOperator extends Component{
 
     render(){
         return (
-            <h2>[This is variable]</h2>
+            <h2>[this is R011_SpreadOperator]</h2>
         )
     }
 }
