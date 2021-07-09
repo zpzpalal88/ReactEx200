481e0900ee77bca4c69a9614c61ef8ddc6386e1f
Parker <zpzpalal88@gmail.com>
2021-07-09 09:06:54 +0900

    APP.JS 수정

---
+++ src/R012_Class&Prototype.js
@@ -0,0 +1,26 @@
+
+
+import React, {Component} from 'react';
+
+class ClassPrototype extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state={};
+
+        
+    }
+    
+
+    componentDidMount(){
+    }
+
+    render(){
+        return (
+            <h2>[this is Class]</h2>
+        )
+    }
+}
+
+
+export default ClassPrototype;
